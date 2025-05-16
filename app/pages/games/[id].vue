6513fd0ca4d7b858abe69172315ea5c09c123da6
nethris <david.abou@epitech.eu>
2025-05-16 12:19:21 -0400

    feat: add game searching and game skills

--- app/pages/games/[id].vue
+++ app/pages/games/[id].vue
@@ -3,7 +3,30 @@ const { data: game } = await useFetch(`/api/games/${useRoute().params.id}`)
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <h1>{{ game.name }}</h1>
+  <div class="flex flex-col py-6">
+    <div class="flex space-x-4 items-center">
+      <NuxtLink
+        to="/games"
+        class="flex items-center justify-center size-10 border-2 border-zinc-200 hover:bg-zinc-100 rounded-full"
+      >
+        <Icon name="ri:arrow-left-s-line" size="20" />
+      </NuxtLink>
+      <NuxtImg
+        :src="game.imageURL"
+        width="64"
+        height="64"
+        class="object-cover rounded-xl"
+      />
+      <h1 class="text-xl font-bold">{{ game.name }}</h1>
+    </div>
+    <p class="my-6">{{ game.description }}</p>
+    <ul class="flex space-x-2">
+      <li
+        v-for="skills of game.game_skills.map((gs: any) => gs.skills)"
+        class="bg-zinc-100 rounded-full px-4 py-2 text-sm font-bold text-zinc-500"
+      >
+        {{ skills.name }}
+      </li>
+    </ul>
   </div>
 </template>
--- app/pages/games/index.vue
+++ app/pages/games/index.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
 const { data: games } = await useFetch('/api/games')
+
+const search = ref('')
+
+const filteredGames = computed(() => {
+  if (!search.value) return games.value
+
+  return games.value?.filter((game) =>
+    game.name.toLowerCase().includes(search.value.toLowerCase())
+  )
+})
 </script>
 
 <template>
   <div class="flex flex-col">
     <form class="py-7">
-      <SearchInput placeholder="Rechercher..." />
+      <SearchInput v-model="search" placeholder="Rechercher..." />
     </form>
     <div
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12"
     >
       <GameCard
-        v-for="game in games"
+        v-for="game in filteredGames"
         :key="game.id"
         :name="game.name"
         :thumbnail="game.imageURL"
